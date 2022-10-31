6f1e3d22221ad6db48b41ea1adcc218606d271a9
Rania Gisela <rania.gisela.ft21@mail.umy.ac.id>
2022-10-31 16:07:31 +0700

    change "arr" with my nickname

--- Program.cs
+++ Program.cs
@@ -9,7 +9,7 @@ namespace Exercise2
     class Program
     {
         //array of integers to hold values
-        private int[] arr = new int[53];
+        private int[] rara = new int[53];
         private int cmp_count = 0; //number of comparasion
         private int mov_count = 0; //number of movements
 
