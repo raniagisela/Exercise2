6ed21e5d2b762915cfd8a91f45d8a312a51a7869
Rania Gisela <rania.gisela.ft21@mail.umy.ac.id>
2022-10-31 16:53:29 +0700

    add static void

--- Program.cs
+++ Program.cs
@@ -120,5 +120,20 @@ namespace Exercise2
         {
             return (n);
         }
+        static void main(string[] args)
+        {
+            //Declaring the object of the class
+            Program myList = new Program();
+            //Acept array elements
+            myList.read();
+            //Calling the sorting function
+            //Frist call to Quick sort Alogarithm
+            myList.q_sort(0, myList.getSize() - 1);
+            //Display sorted array
+            myList.display();
+            // to exit from the console
+            Console.WriteLine("\n\nPress Enter to exit. ");
+            Console.Read();
+        }
     }
 }
