b5618c2cca3c0796d1d0392aa950e11d277aa440
Rania Gisela <rania.gisela.ft21@mail.umy.ac.id>
2022-10-31 16:01:20 +0700

    add array of intergers

--- Program.cs
+++ Program.cs
@@ -6,4 +6,11 @@ using System.Threading.Tasks;
 
 namespace Exercise2
 {
+    class Program
+    {
+        //array of integers to hold values
+        private int[] arr = new int[53];
+        private int cmp_count = 0; //number of comparasion
+        private int mov_count = 0; //number of movements
+    }
 } 
