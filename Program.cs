29c45b864ebdcab4040cdb01ae4f71b1ff00bee8
Rania Gisela <rania.gisela.ft21@mail.umy.ac.id>
2022-10-31 16:53:04 +0700

    add int getSize

--- Program.cs
+++ Program.cs
@@ -116,5 +116,9 @@ namespace Exercise2
             Console.WriteLine("\nNumber of comparisons: " + cmp_count);
             Console.WriteLine("\nNumber of data movemenets: " + mov_count);
         }
+        int getSize()
+        {
+            return (n);
+        }
     }
 }
